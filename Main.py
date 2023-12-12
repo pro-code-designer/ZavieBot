f2baefd5475c657160f7abfe6aa93d45fa3785bd
pro-code-designer <46442168+pro-code-designer@users.noreply.github.com>
2023-12-12 15:36:54 +0330

    Comment Main.py

--- Main.py
+++ Main.py
@@ -1,45 +1,48 @@
-from typing import Final
-from telegram import Update
-from telegram.ext import Updater, MessageHandler, CommandHandler, filters, ContextTypes, Application
 import re
 import Database as db
+from typing import Final
 from Makepdf import makepdf
+from telegram import Update
+from telegram.ext import Updater, MessageHandler, CommandHandler, filters, ContextTypes, Application
 
 
+# Function to validate phone numbers
 def validate_number(number):
     pattern = r'09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}'
     return re.match(pattern, number) is not None
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Function to validate names (supports both English and Persian names)
 def validate_name(name):
     pattern1 = r'^[A-Za-z\s]+$'
     pattern2 = r'^[\u0600-\u06FF\s]+$'
     return (re.match(pattern1, name) is not None) or (re.match(pattern2, name) is not None)
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from BotFather
 TOKEN: Final = '6635980529:AAECa563vQWDpsXx73yTXDFjDOiQ3WZKjRk'
 BOT_USERNAME: Final = 'ZzavieBot'
 
-# Lets us use the /start command
-
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to start the bot
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello there! I\'m a bot. What\'s up?')
 
-
-# Lets us use the /help command
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to get help
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Try typing anything and I will do my best to respond!')
 
-
-# Lets us use the /custom command
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to sign up
 async def signup_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Print user and chat information for debugging
     print(context._user_id, "\n", context._chat_id, context.args)
     i = 0
     name = ''
     fname = ''
     number = ''
+    # Extracting name, last name, and phone number from the command arguments
     for item in context.args:
         if(item != ','):
             if(i == 0):
@@ -52,8 +55,10 @@ async def signup_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         else:
             i += 1
     print(f"name ={name}  family name={fname}    phone number={number}")
+    # Check if the user has already signed up
     if(db.get_by_id(context._user_id) != None):
         await update.message.reply_text('you signed up before.')
+    # Validate and check the format of the entered name, last name, and phone number
     elif(not validate_name(name)):
         await update.message.reply_text(f'it is not a valid name you put {name} as a name!')
     elif (not validate_name(fname)):
@@ -61,16 +66,20 @@ async def signup_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif (not validate_number(number)):
         await update.message.reply_text(f'it is not a valid phone number you put {number} as a phone number!')
     else:
+        # Add the user to the database
         db.add_user(context._user_id, name, fname, number)
         await update.message.reply_text('signed up successfully.')
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to edit user information
 async def edit_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Print user and chat information for debugging
     print(context._user_id, "\n", context._chat_id, context.args)
     i = 0
     name = ''
     fname = ''
     number = ''
+    # Extracting name, last name, and phone number from the command arguments
     for item in context.args:
         if(item != ','):
             if(i == 0):
@@ -83,8 +92,10 @@ async def edit_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         else:
             i += 1
     print(f"name ={name}  family name={fname}    phone number={number}")
+    # Check if the user has signed up before
     if(db.get_by_id(context._user_id) == None):
         await update.message.reply_text('sign up first')
+    # Validate and check the format of the entered name, last name, and phone number
     elif(not validate_name(name)):
         await update.message.reply_text(f'it is not a valid name you put {name} as a name!')
     elif (not validate_name(fname)):
@@ -92,12 +103,16 @@ async def edit_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif (not validate_number(number)):
         await update.message.reply_text(f'it is not a valid phone number you put {number} as a phone number!')
     else:
+        # Update user data in the database
         db.change_user_data(context._user_id, name, fname, number)
         await update.message.reply_text('edited successfully.')
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to show user information
 async def show_me_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Retrieve user data from the database
     data = db.get_by_id(context._user_id)
+    # Check if user data is available
     if(data != None):
         name = data[1]
         fname = data[2]
@@ -112,11 +127,13 @@ async def show_me_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
             stay = 'stay'
         else:
             stay = 'do not stay'
+        # Display user information
         await update.message.reply_text(f'your name ={name}\nfamily name={fname}\nphone number={number}\nand you {permi} to stay in night and you {stay} tonight ')
     else:
         await update.message.reply_text(f'ther is no data for you make sure you signed up before!')
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to change permission
 async def permi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     number = context.args[0]
     data = db.get_by_number(number)
@@ -137,7 +154,8 @@ async def permi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text(f'ther is no data for the number you search!')
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to change stay status
 async def stay_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     data = db.get_by_id(context._user_id)
     if(data != None):
@@ -156,32 +174,36 @@ async def stay_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text(f'ther is no data for the number you search!')
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Error handler
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} caused error {context.error}')
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to get a PDF containing all users
 async def get_all_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ans = makepdf("All_User", db.get_all_user())
     if(ans):
         document = open("./All_User.pdf", "rb")
         await update.message.reply_document(document, "همه کاربران")
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to get a PDF containing users with permission
 async def get_by_permission_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ans = makepdf("Permissioned_User", db.get_by_night_permi())
     if(ans):
         document = open("./Permissioned_User.pdf", "rb")
         await update.message.reply_document(document, "کاربرانی که اجازه شب ماندن دارند")
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+# Command to get a PDF containing users with night stay
 async def get_by_night_stay_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ans = makepdf("Night_Stay_User", db.get_by_night_stay())
     if(ans):
         document = open("./Night_Stay_User.pdf", "rb")
         await update.message.reply_document(document, "کاربرانی که شب می مانند")
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------
 # Run the program
 if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
